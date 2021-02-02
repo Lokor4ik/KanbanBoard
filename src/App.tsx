a1235fdd850844545a227dfd16136d2c03b7fd73
Lokor4ik <lokorinfantem@gmail.com>
2021-02-02 21:54:50 +0200

    changed types and logic board

--- src/App.tsx
+++ src/App.tsx
@@ -1,53 +1,13 @@
-import { useCallback, useState } from 'react';
+import { useState } from 'react';
 import { v4 as uuidv4 } from 'uuid';
 
-import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
+import { DragDropContext, Droppable, Draggable, DropResult } from 'react-beautiful-dnd';
 
 import './App.css';
 
-interface NotDraggingStyle {
-  transform?: string;
-  transition?: 'none';
-}
-interface DraggingStyle {
-  position: 'fixed';
-  top: number;
-  left: number;
-  boxSizing: 'border-box';
-  width: number;
-  height: number;
-  transition: 'none';
-  transform?: string;
-  zIndex: number;
-  opacity?: number;
-  pointerEvents: 'none';
-}
-interface ItemsModel {
-  id: string;
-  content: string;
-}
-
-type DraggableStyleModel = DraggingStyle | NotDraggingStyle | undefined;
-
-const getItems = (count: number) =>
-  Array.from({ length: count }, (_, k) => k).map((k) => ({
-    id: uuidv4(),
-    content: `item ${k}`,
-  }));
-
-const reorder = (list: ItemsModel[], startIndex: number, endIndex: number) => {
-  const result = Array.from(list);
-  const [removed] = result.splice(startIndex, 1);
-  result.splice(endIndex, 0, removed);
-
-  return result;
-};
-
-const grid = 8;
-
-const getItemStyle = (isDragging: boolean, draggableStyle: DraggableStyleModel) => ({
-  padding: grid * 2,
-  margin: `0 0 ${grid}px 0`,
+/* const getItemStyle = (isDragging: boolean, draggableStyle: DraggableStyleModel) => ({
+  padding: 16,
+  margin: '0 0 8px 0',
 
   // change background colour if dragging
   background: isDragging ? 'lightgreen' : 'grey',
@@ -58,88 +18,134 @@ const getItemStyle = (isDragging: boolean, draggableStyle: DraggableStyleModel) 
 
 const getListStyle = (isDraggingOver: boolean) => ({
   background: isDraggingOver ? 'lightblue' : 'lightgrey',
-  padding: grid,
+  padding: 8,
   width: 250,
-});
+}); */
+
+const itemsFromBackend = [
+  { id: uuidv4(), content: 'First task' },
+  { id: uuidv4(), content: 'Second task' },
+  { id: uuidv4(), content: 'Third task' },
+  { id: uuidv4(), content: 'Fourth task' },
+  { id: uuidv4(), content: 'Fifth task' },
+];
+
+const columnsFromBackend = {
+  [uuidv4()]: {
+    name: 'Requested',
+    items: itemsFromBackend,
+  },
+  [uuidv4()]: {
+    name: 'To do',
+    items: [],
+  },
+  [uuidv4()]: {
+    name: 'In Progress',
+    items: [],
+  },
+  [uuidv4()]: {
+    name: 'Done',
+    items: [],
+  },
+};
 
 function App() {
-  const [items, setItems] = useState<ItemsModel[]>(getItems(10));
-  const onDragEnd = useCallback(
-    (result) => {
-      // dropped outside the list
-      if (!result.destination) {
-        return;
-      }
+  const [columns, setColumns] = useState(columnsFromBackend);
 
-      const newItems = reorder(items, result.source.index, result.destination.index);
+  const onDragEnd = (result: DropResult) => {
+    if (!result.destination) return;
+    const { source, destination } = result;
 
-      setItems([...newItems]);
-    },
-    [items]
-  );
+    if (source.droppableId !== destination.droppableId) {
+      const sourceColumn = columns[source.droppableId];
+      const destColumn = columns[destination.droppableId];
+      const sourceItems = [...sourceColumn.items];
+      const destItems = [...destColumn.items];
+      const [removed] = sourceItems.splice(source.index, 1);
+      destItems.splice(destination.index, 0, removed);
+      setColumns({
+        ...columns,
+        [source.droppableId]: {
+          ...sourceColumn,
+          items: sourceItems,
+        },
+        [destination.droppableId]: {
+          ...destColumn,
+          items: destItems,
+        },
+      });
+    } else {
+      const column = columns[source.droppableId];
+      const copiedItems = [...column.items];
+      const [removed] = copiedItems.splice(source.index, 1);
+      copiedItems.splice(destination.index, 0, removed);
+      setColumns({
+        ...columns,
+        [source.droppableId]: {
+          ...column,
+          items: copiedItems,
+        },
+      });
+    }
+  };
 
   return (
-    <DragDropContext onDragEnd={onDragEnd}>
-      <div className="board-wrapper">
-        <Droppable droppableId="droppable-1">
-          {(providedUpper, snapshotUpper) => (
-            <div
-              {...providedUpper.droppableProps}
-              ref={providedUpper.innerRef}
-              style={getListStyle(snapshotUpper.isDraggingOver)}
-            >
-              {items.map((item: ItemsModel, index: number) => (
-                <Draggable key={item.id} draggableId={item.id} index={index}>
-                  {(providedLower, snapshotLower) => (
-                    <div
-                      ref={providedLower.innerRef}
-                      {...providedLower.draggableProps}
-                      {...providedLower.dragHandleProps}
-                      style={getItemStyle(
-                        snapshotLower.isDragging,
-                        providedLower.draggableProps.style
-                      )}
-                    >
-                      {item.content}
-                    </div>
-                  )}
-                </Draggable>
-              ))}
-              {providedUpper.placeholder}
+    <div className="board-wrapper">
+      <DragDropContext onDragEnd={(result) => onDragEnd(result)}>
+        {Object.entries(columns).map(([columnId, column]) => (
+          <div
+            style={{
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+            }}
+            key={columnId}
+          >
+            <h2>{column.name}</h2>
+            <div style={{ margin: 8 }}>
+              <Droppable droppableId={columnId} key={columnId}>
+                {(providedUpper, snapshotUpper) => (
+                  <div
+                    {...providedUpper.droppableProps}
+                    ref={providedUpper.innerRef}
+                    style={{
+                      background: snapshotUpper.isDraggingOver ? 'lightblue' : 'lightgrey',
+                      padding: 4,
+                      width: 250,
+                      minHeight: 500,
+                    }}
+                  >
+                    {column.items.map((item, index) => (
+                      <Draggable key={item.id} draggableId={item.id} index={index}>
+                        {(providedLower, snapshotLower) => (
+                          <div
+                            ref={providedLower.innerRef}
+                            {...providedLower.draggableProps}
+                            {...providedLower.dragHandleProps}
+                            style={{
+                              userSelect: 'none',
+                              padding: 16,
+                              margin: '0 0 8px 0',
+                              minHeight: '50px',
+                              backgroundColor: snapshotLower.isDragging ? '#263B4A' : '#456C86',
+                              color: 'white',
+                              ...providedLower.draggableProps.style,
+                            }}
+                          >
+                            {item.content}
+                          </div>
+                        )}
+                      </Draggable>
+                    ))}
+                    {providedUpper.placeholder}
+                  </div>
+                )}
+              </Droppable>
             </div>
-          )}
-        </Droppable>
-
-        <Droppable droppableId="droppable-2">
-          {(providedUpper, snapshotUpper) => (
-            <div
-              {...providedUpper.droppableProps}
-              ref={providedUpper.innerRef}
-              style={getListStyle(snapshotUpper.isDraggingOver)}
-            >
-              {items.map((item: ItemsModel, index: number) => (
-                <Draggable key={item.id} draggableId={item.id} index={index}>
-                  {(providedLower, snapshotLower) => (
-                    <div
-                      ref={providedLower.innerRef}
-                      {...providedLower.draggableProps}
-                      {...providedLower.dragHandleProps}
-                      style={getItemStyle(
-                        snapshotLower.isDragging,
-                        providedLower.draggableProps.style
-                      )}
-                    >
-                      {item.content}
-                    </div>
-                  )}
-                </Draggable>
-              ))}
-              {providedUpper.placeholder}
-            </div>
-          )}
-        </Droppable>
-      </div>
-    </DragDropContext>
+          </div>
+        ))}
+      </DragDropContext>
+    </div>
   );
 }
 
