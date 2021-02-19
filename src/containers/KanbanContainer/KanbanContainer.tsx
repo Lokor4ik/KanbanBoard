22bdc3185ad8965a62c001a9ed18499908d6c260
Lokor4ik <lokorinfantem@gmail.com>
2021-02-19 16:50:41 +0200

    implemented redux store in kanban container

--- src/containers/KanbanContainer/KanbanContainer.tsx
+++ src/containers/KanbanContainer/KanbanContainer.tsx
@@ -1,89 +1,41 @@
-import { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { v4 as uuidv4 } from 'uuid';
 import { DragDropContext, DropResult } from 'react-beautiful-dnd';
 
 import KanbanColumn from 'components/KanbanColumn/KanbanColumn';
 import KanbanColumnContent from 'components/KanbanColumnContent/KanbanColumnContent';
 
-import { changeCardDistination } from 'store/kanban/action';
+import { changeCardColumn, changeCardPosition } from 'store/kanban/action';
 
 import './KanbanContainer.scss';
-
-const itemsFromBackend = [
-  { id: uuidv4(), content: 'First task' },
-  { id: uuidv4(), content: 'Second task' },
-  { id: uuidv4(), content: 'Third task' },
-  { id: uuidv4(), content: 'Fourth task' },
-  { id: uuidv4(), content: 'Fifth task' },
-];
-
-const columnsFromBackend = {
-  [uuidv4()]: {
-    name: 'Requested',
-    items: itemsFromBackend,
-  },
-  [uuidv4()]: {
-    name: 'To do',
-    items: [],
-  },
-  [uuidv4()]: {
-    name: 'In Progress',
-    items: [],
-  },
-  [uuidv4()]: {
-    name: 'Done',
-    items: [],
-  },
-};
+import { RootState } from 'store/rootReducer';
 
 const KanbanContainer = () => {
-  const [columns, setColumns] = useState(columnsFromBackend);
-
-  const { columns } = useSelector((state) => state);
+  const { columns } = useSelector((state: RootState) => state.kanban);
 
   const dispatch = useDispatch();
 
   const onDragEnd = (result: DropResult) => {
-    dispatch(changeCardDistination(result));
     if (!result.destination) return;
+
     const { source, destination } = result;
 
     if (source.droppableId !== destination.droppableId) {
-      const sourceColumn = columns[source.droppableId];
-      const destColumn = columns[destination.droppableId];
-
-      const sourceItems = [...sourceColumn.items];
-      const destItems = [...destColumn.items];
-
-      const [removed] = sourceItems.splice(source.index, 1);
-      destItems.splice(destination.index, 0, removed);
-
-      setColumns({
-        ...columns,
-        [source.droppableId]: {
-          ...sourceColumn,
-          items: sourceItems,
-        },
-        [destination.droppableId]: {
-          ...destColumn,
-          items: destItems,
-        },
-      });
+      dispatch(
+        changeCardColumn({
+          srcDroppableId: source.droppableId,
+          destDroppableId: destination.droppableId,
+          srcIndex: source.index,
+          destIndex: destination.index,
+        })
+      );
     } else {
-      const column = columns[source.droppableId];
-      const copiedItems = [...column.items];
-
-      const [removed] = copiedItems.splice(source.index, 1);
-      copiedItems.splice(destination.index, 0, removed);
-
-      setColumns({
-        ...columns,
-        [source.droppableId]: {
-          ...column,
-          items: copiedItems,
-        },
-      });
+      dispatch(
+        changeCardPosition({
+          srcDroppableId: source.droppableId,
+          srcIndex: source.index,
+          destIndex: destination.index,
+        })
+      );
     }
   };
 
