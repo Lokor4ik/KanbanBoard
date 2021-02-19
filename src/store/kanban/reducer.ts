30567705e0757f517e906d63074b6395feef3aac
Lokor4ik <lokorinfantem@gmail.com>
2021-02-19 16:51:27 +0200

    added reducer logic

--- src/store/kanban/reducer.ts
+++ src/store/kanban/reducer.ts
@@ -1,14 +1,40 @@
-import { SET_COLUMNS } from './types';
+import { v4 as uuidv4 } from 'uuid';
+
+import { SET_CARD_DESTINATION, KanbanActionTypes } from './types';
 
 const initialState = {
-  columns: [],
+  columns: {
+    [uuidv4()]: {
+      name: 'Requested',
+      items: [
+        { id: uuidv4(), content: 'First task' },
+        { id: uuidv4(), content: 'Second task' },
+        { id: uuidv4(), content: 'Third task' },
+        { id: uuidv4(), content: 'Fourth task' },
+        { id: uuidv4(), content: 'Fifth task' },
+      ],
+    },
+    [uuidv4()]: {
+      name: 'To do',
+      items: [],
+    },
+    [uuidv4()]: {
+      name: 'In Progress',
+      items: [],
+    },
+    [uuidv4()]: {
+      name: 'Done',
+      items: [],
+    },
+  },
 };
 
-export default function rootReducer(state = initialState, action) {
+export default function rootReducer(state = initialState, action: KanbanActionTypes) {
   switch (action.type) {
-    case SET_COLUMNS: {
+    case SET_CARD_DESTINATION: {
       return {
         ...state,
+        columns: action.payload.modifiedColumns,
       };
     }
     default:
