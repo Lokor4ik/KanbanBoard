50127b79f8f208feb09ee1dab5c6be37d5964c02
Lokor4ik <lokorinfantem@gmail.com>
2021-02-19 16:51:09 +0200

    added actions

--- src/store/kanban/action.ts
+++ src/store/kanban/action.ts
@@ -1,15 +1,71 @@
-import { ThunkDispatch } from 'redux-thunk';
-import { AnyAction } from 'redux';
+import { ThunkAction } from 'redux-thunk';
+import { Action } from 'redux';
 
-import { SET_CARD_COLUMN, SET_CARD_POSITION } from './types';
+import { RootState } from 'store/rootReducer';
+import { SET_CARD_DESTINATION, ParamsChangeCardColumn, ParamsChangeCardPosition } from './types';
 
-export const changeCardDistination = () => async (dispatch: ThunkDispatch<{}, {}, AnyAction>) => {
+export const changeCardColumn = ({
+  srcDroppableId,
+  destDroppableId,
+  srcIndex,
+  destIndex,
+}: ParamsChangeCardColumn): ThunkAction<void, RootState, null, Action<string>> => async (
+  dispatch,
+  getState
+) => {
+  const { columns } = getState().kanban;
+
+  const sourceColumn = columns[srcDroppableId];
+  const destColumn = columns[destDroppableId];
+  const sourceItems = [...sourceColumn.items];
+  const destItems = [...destColumn.items];
+
+  const [removed] = sourceItems.splice(srcIndex, 1);
+  destItems.splice(destIndex, 0, removed);
+
+  const modifiedColumns = {
+    ...columns,
+    [srcDroppableId]: {
+      ...sourceColumn,
+      items: sourceItems,
+    },
+    [destDroppableId]: {
+      ...destColumn,
+      items: destItems,
+    },
+  };
+
   dispatch({
-    type: SET_CARD_COLUMN,
-    payload: {},
+    type: SET_CARD_DESTINATION,
+    payload: { modifiedColumns },
   });
+};
+
+export const changeCardPosition = ({
+  srcDroppableId,
+  srcIndex,
+  destIndex,
+}: ParamsChangeCardPosition): ThunkAction<void, RootState, null, Action<string>> => async (
+  dispatch,
+  getState
+) => {
+  const { columns } = getState().kanban;
+
+  const column = columns[srcDroppableId];
+  const copiedItems = [...column.items];
+  const [removed] = copiedItems.splice(srcIndex, 1);
+  copiedItems.splice(destIndex, 0, removed);
+
+  const modifiedColumns = {
+    ...columns,
+    [srcDroppableId]: {
+      ...column,
+      items: copiedItems,
+    },
+  };
+
   dispatch({
-    type: SET_CARD_POSITION,
-    payload: {},
+    type: SET_CARD_DESTINATION,
+    payload: { modifiedColumns },
   });
 };
