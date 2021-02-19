10982b0464bb6458276cf1598ee6cf47737da19f
Lokor4ik <lokorinfantem@gmail.com>
2021-02-19 16:51:44 +0200

    added types for kanban state & actions

--- src/store/kanban/types.ts
+++ src/store/kanban/types.ts
@@ -1,9 +1,7 @@
-export const SET_CARD_COLUMN = 'SET_CARD_COLUMN';
-export const SET_CARD_POSITION = 'SET_CARD_POSITION';
+export const SET_CARD_DESTINATION = 'SET_CARD_DESTINATION';
 
 interface ChangeCardDistinationPayload {
-  sourceDroppableId: string;
-  destinationDroppableId: string;
+  modifiedColumns: TypeKanbanColumns;
 }
 
 interface ChangeCardDistinationAction {
@@ -12,3 +10,20 @@ interface ChangeCardDistinationAction {
 }
 
 export type KanbanActionTypes = ChangeCardDistinationAction;
+
+export interface TypeKanbanColumns {
+  [key: string]: {
+    name: string;
+    items: Array<{ id: string; content: string }>;
+  };
+}
+
+export type ParamsChangeCardPosition = {
+  srcDroppableId: string;
+  srcIndex: number;
+  destIndex: number;
+};
+
+export type ParamsChangeCardColumn = ParamsChangeCardPosition & {
+  destDroppableId: string;
+};
