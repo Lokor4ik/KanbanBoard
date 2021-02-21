7ddc13cadf174ef2a5d31d56fa8fe7ea32eb46cf
Lokor4ik <lokorinfantem@gmail.com>
2021-02-21 14:47:36 +0200

    added NotFound page content

---
+++ src/pages/NotFound/NotFound.tsx
@@ -0,0 +1,12 @@
+import './NotFound.scss';
+
+const NotFound = () => (
+  <div className="not-found">
+    <div className="not-found__content">
+      <h1>404 Error Oops!</h1>
+      <p>We can&lsquo;t find that page.</p>
+    </div>
+  </div>
+);
+
+export default NotFound;
