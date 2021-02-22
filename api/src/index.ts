5f54be66afb163255c3fccf84e00f0f306973860
Lokor4ik <lokorinfantem@gmail.com>
2021-02-22 17:48:06 +0200

    test commit

--- api/src/index.ts
+++ api/src/index.ts
@@ -5,5 +5,5 @@ const PORT = 8000;
 app.get('/', (_, res) => res.send('Express + TypeScript Server'));
 app.listen(PORT, () => {
   console.log(`Server is running at https://localhost:${PORT}`);
-  console.log(`Server is running at https://localhost:${PORT}`);
+  console.log(`Server is running at https://localhost:${PORT}`)
 });
