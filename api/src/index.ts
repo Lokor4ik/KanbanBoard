5fe237fe2a214651a69facb498d9c69cd0cbd3ce
Lokor4ik <lokorinfantem@gmail.com>
2021-02-26 00:02:30 +0200

    implemented cors and dotenv

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,8 +1,32 @@
+import dotenv from 'dotenv';
 import express from 'express';
+import cors, { CorsOptions } from 'cors';
+
+dotenv.config();
+
+const { API_PORT } = process.env;
 
 const app = express();
-const PORT = 8000;
-app.get('/', (_, res) => res.send('Express + TypeScript Server'));
-app.listen(PORT, () => {
-  console.log(`Server is running at https://localhost:${PORT}`);
+
+const corsOptions: CorsOptions = {
+  origin: ['http://localhost:3000'],
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (_, res) => {
+  res.send('Hello World!');
 });
+
+if (require.main === module) {
+  try {
+    app.listen(API_PORT, () => {
+      console.log(`API server listening on port ${API_PORT}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
