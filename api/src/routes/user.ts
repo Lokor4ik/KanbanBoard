7dd48b2f967a56e60611a95e505be85db581ba46
Lokor4ik <lokorinfantem@gmail.com>
2021-02-28 22:01:30 +0200

    added user route

---
+++ api/src/routes/user.ts
@@ -0,0 +1,19 @@
+import express from 'express';
+import { check } from 'express-validator';
+
+import userController from 'controllers/user';
+
+const router = express.Router();
+
+const middlewares = [
+  check('name', 'Name is required').not().isEmpty(),
+  check('email', 'Please include a valid email').isEmail(),
+  check('password', 'Please enter a password with 6 or more characters').isLength({ min: 6 }),
+];
+
+// @route  POST api/users
+// @desc   Register user
+// @access Public
+router.post('/user', middlewares, userController.register);
+
+export default router;
