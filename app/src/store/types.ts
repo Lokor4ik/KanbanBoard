edca7afd34b95ccd25889792d37c92898c24ad97
Lokor4ik <lokorinfantem@gmail.com>
2021-03-01 20:52:15 +0200

    added shared thunk action

--- app/src/store/types.ts
+++ app/src/store/types.ts
@@ -1,3 +1,8 @@
+import { ThunkAction } from 'redux-thunk';
+import { Action } from 'redux';
+
 import rootReducer from './rootReducer';
 
 export type RootState = ReturnType<typeof rootReducer>;
+
+export type RootThunkAction = ThunkAction<void, RootState, null, Action<string>>;
