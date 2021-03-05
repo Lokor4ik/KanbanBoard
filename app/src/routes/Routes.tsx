9f9c6ab40da48115adfa4feab4de03264e02b00a
Lokor4ik <lokorinfantem@gmail.com>
2021-03-05 22:18:43 +0200

    added routed for non authenicated uer

--- app/src/routes/Routes.tsx
+++ app/src/routes/Routes.tsx
@@ -3,11 +3,23 @@ import { Switch, Route } from 'react-router-dom';
 import Projects from 'pages/Projects/Projects';
 import Kanban from 'pages/Kanban/Kanban';
 import NotFound from 'pages/NotFound/NotFound';
+import Landing from 'pages/Landing/Landing';
 
-export const useRoutes = () => (
-  <Switch>
-    <Route exact path="/projects" component={Projects} />
-    <Route exact path="/projects/:id/boards/:id" component={Kanban} />
-    <Route component={NotFound} />
-  </Switch>
-);
+export const useRoutes = (isAuthenticated: boolean) => {
+  if (isAuthenticated) {
+    return (
+      <Switch>
+        <Route exact path="/projects" component={Projects} />
+        <Route exact path="/projects/:id/boards/:id" component={Kanban} />
+        <Route component={NotFound} />
+      </Switch>
+    );
+  }
+
+  return (
+    <Switch>
+      <Route exact path="/" component={Landing} />
+      <Route path="*" component={NotFound} />
+    </Switch>
+  );
+};
