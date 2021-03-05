67d7dcafe2deff23a979f2207ddd53ae50993396
Lokor4ik <lokorinfantem@gmail.com>
2021-03-05 22:22:22 +0200

    fixed name url and improved import folder

--- api/src/routes/user.ts
+++ api/src/routes/user.ts
@@ -1,7 +1,7 @@
 import express from 'express';
 import { check } from 'express-validator';
 
-import userController from 'controllers/user';
+import userController from 'controllers/user/user';
 
 const router = express.Router();
 
@@ -14,6 +14,6 @@ const middlewares = [
 // @route  POST api/users
 // @desc   Register user
 // @access Public
-router.post('/user', middlewares, userController.register);
+router.post('/', middlewares, userController.register);
 
 export default router;
