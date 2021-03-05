c6d0758bcbf6f7ba8a0b031c6f2a4cacdcc41f3e
Lokor4ik <lokorinfantem@gmail.com>
2021-03-05 22:21:37 +0200

    fixed name of routes

--- api/src/routes/index.ts
+++ api/src/routes/index.ts
@@ -4,8 +4,8 @@ import authRoute from './auth';
 import userRoute from './user';
 
 function routes(app: Application) {
-  app.use('/api', authRoute);
-  app.use('/api', userRoute);
+  app.use('/api/auth', authRoute);
+  app.use('/api/user', userRoute);
 }
 
 export default routes;
