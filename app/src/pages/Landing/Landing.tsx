c8ef690cc9c5051a8091a9f8784acb36263c36e9
Lokor4ik <lokorinfantem@gmail.com>
2021-03-05 22:19:07 +0200

    added Landing component

---
+++ app/src/pages/Landing/Landing.tsx
@@ -0,0 +1,14 @@
+import MainLayout from 'layouts/MainLayout/MainLayout';
+
+import './Landing.scss';
+
+const NotFound = () => (
+  <MainLayout sectionName="landing">
+    <div className="landing__content">
+      <h1>Landing</h1>
+      <p>Buttons</p>
+    </div>
+  </MainLayout>
+);
+
+export default NotFound;
