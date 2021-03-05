8cee82892d1783ce75fe0d29b0b08c24175350b5
Lokor4ik <lokorinfantem@gmail.com>
2021-03-05 22:21:23 +0200

    added new route

--- api/src/routes/auth.ts
+++ api/src/routes/auth.ts
@@ -1,15 +1,22 @@
 import express from 'express';
 import { check } from 'express-validator';
 
-import authController from 'controllers/auth';
+import auth from 'middleware/auth/auth';
+
+import authController from 'controllers/auth/auth';
 
 const router = express.Router();
 
 const middlewares = [check('email', 'Please include a valid email').isEmail(), check('password', 'Password is required').exists()];
 
+// @route  GET api/auth
+// @desc   Load user
+// @access Public
+router.get('/', auth, authController.load);
+
 // @route  POST api/auth
 // @desc   Authenticate user & get token
 // @access Public
-router.post('/auth', middlewares, authController.authenticate);
+router.post('/', middlewares, authController.authenticate);
 
 export default router;
