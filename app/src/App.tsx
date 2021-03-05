075ef1adb1922dd2c219feb8096af4ad44b2d5b6
Lokor4ik <lokorinfantem@gmail.com>
2021-03-05 22:20:19 +0200

    added validation for authenicated user

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,11 +1,31 @@
+import { useEffect } from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
+
+import Loader from 'shared/Loader/Loader';
+
+import { RootState } from 'store/types';
+import { loadUser } from 'store/auth/action';
 
 import { useRoutes } from 'routes/Routes';
 
 import './App.scss';
 
 const App = () => {
-  const routes = useRoutes();
+  const { isAuthenticated, loading } = useSelector((state: RootState) => state.auth);
+
+  const dispatch = useDispatch();
+  const routes = useRoutes(isAuthenticated);
+
+  useEffect(() => {
+    if (!isAuthenticated) {
+      dispatch(loadUser());
+    }
+  }, [dispatch, isAuthenticated]);
+
+  if (loading) {
+    return <Loader />;
+  }
 
   return (
     <Router>
