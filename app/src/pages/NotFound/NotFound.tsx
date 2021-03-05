0450e80fd03c4a3c034cea5ae671c94f619f2083
Lokor4ik <lokorinfantem@gmail.com>
2021-03-05 22:22:39 +0200

    fixed JSX structure

--- app/src/pages/NotFound/NotFound.tsx
+++ app/src/pages/NotFound/NotFound.tsx
@@ -1,12 +1,14 @@
+import MainLayout from 'layouts/MainLayout/MainLayout';
+
 import './NotFound.scss';
 
 const NotFound = () => (
-  <div className="not-found">
+  <MainLayout sectionName="not-found">
     <div className="not-found__content">
       <h1>404 Error Oops!</h1>
       <p>We can&lsquo;t find that page.</p>
     </div>
-  </div>
+  </MainLayout>
 );
 
 export default NotFound;
