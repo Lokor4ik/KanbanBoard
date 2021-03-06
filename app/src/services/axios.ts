169c50b58530a2efa6231966bce1dc811998bd04
Lokor4ik <lokorinfantem@gmail.com>
2021-03-06 16:23:49 +0200

    modified axios service

--- app/src/services/axios.ts
+++ app/src/services/axios.ts
@@ -1,15 +1,15 @@
 const request = async (url: string, method = 'GET', body: null | string, headers = {}) => {
   try {
-    const response = await fetch(url, { method, body, headers });
+    const response = await fetch(`http://localhost:9090${url}`, { method, headers, body });
     const data = await response.json();
 
     if (!response.ok) {
-      throw new Error(data.message || 'Something went wrong');
+      throw data.errors || new Error('Something went wrong');
     }
 
     return data;
   } catch (e) {
-    console.error(e.message);
+    console.error(e.message || 'Something went wrong');
     throw e;
   }
 };
