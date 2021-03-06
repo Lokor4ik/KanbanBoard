ea54e4155ecabae6f2870e07af1348680a876aa3
Lokor4ik <lokorinfantem@gmail.com>
2021-03-06 16:23:23 +0200

    added Sign pages

---
+++ app/src/pages/SignIn/SignIn.tsx
@@ -0,0 +1,11 @@
+import MainLayout from 'layouts/MainLayout/MainLayout';
+
+import SignInContainer from 'containers/SignIn/SignIn';
+
+const SignIn = () => (
+  <MainLayout sectionName="signin">
+    <SignInContainer />
+  </MainLayout>
+);
+
+export default SignIn;
---
+++ app/src/pages/SignUp/SignUp.tsx
@@ -0,0 +1,11 @@
+import MainLayout from 'layouts/MainLayout/MainLayout';
+
+import SignUpContainer from 'containers/SignUp/SignUp';
+
+const SignUp = () => (
+  <MainLayout sectionName="signup">
+    <SignUpContainer />
+  </MainLayout>
+);
+
+export default SignUp;
