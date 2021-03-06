8f751fa5e74ea53154905eadee0eb8a8c559571b
Lokor4ik <lokorinfantem@gmail.com>
2021-03-06 16:19:49 +0200

    added mUI semantic

--- app/src/pages/NotFound/NotFound.tsx
+++ app/src/pages/NotFound/NotFound.tsx
@@ -1,14 +1,28 @@
+import Typography from '@material-ui/core/Typography';
 import MainLayout from 'layouts/MainLayout/MainLayout';
+import { makeStyles } from '@material-ui/core/styles';
 
 import './NotFound.scss';
 
-const NotFound = () => (
-  <MainLayout sectionName="not-found">
-    <div className="not-found__content">
-      <h1>404 Error Oops!</h1>
-      <p>We can&lsquo;t find that page.</p>
-    </div>
-  </MainLayout>
-);
+const useStyles = makeStyles({
+  h5: {
+    fontWeight: 600,
+  },
+});
+
+const NotFound = () => {
+  const classes = useStyles();
+
+  return (
+    <MainLayout sectionName="notfound">
+      <div className="notfound__content">
+        <Typography variant="h5" className={classes.h5}>
+          404 Error Oops!
+        </Typography>
+        <Typography variant="subtitle1">We can&lsquo;t find that page.</Typography>
+      </div>
+    </MainLayout>
+  );
+};
 
 export default NotFound;
