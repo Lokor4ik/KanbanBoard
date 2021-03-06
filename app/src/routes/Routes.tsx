5b5b133cbdff889b900c1b8a5ba67543db0698ca
Lokor4ik <lokorinfantem@gmail.com>
2021-03-07 00:08:30 +0200

    added new routes and check for arrival from

--- app/src/routes/Routes.tsx
+++ app/src/routes/Routes.tsx
@@ -1,4 +1,4 @@
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, Redirect, useLocation } from 'react-router-dom';
 
 import Projects from 'pages/Projects/Projects';
 import Kanban from 'pages/Kanban/Kanban';
@@ -7,12 +7,17 @@ import Landing from 'pages/Landing/Landing';
 import SignUp from 'pages/SignUp/SignUp';
 import SignIn from 'pages/SignIn/SignIn';
 
+const pathsFromAuth = ['/login', '/signup', '/'];
+
 export const useRoutes = (isAuthenticated: boolean) => {
+  const location = useLocation();
+
   if (isAuthenticated) {
     return (
       <Switch>
         <Route exact path="/projects" component={Projects} />
         <Route exact path="/projects/:id/boards/:id" component={Kanban} />
+        {pathsFromAuth.includes(location.pathname) && <Redirect to="/projects" />}
         <Route component={NotFound} />
       </Switch>
     );
@@ -22,8 +27,9 @@ export const useRoutes = (isAuthenticated: boolean) => {
     <Switch>
       <Route exact path="/" component={Landing} />
       <Route exact path="/signup" component={SignUp} />
-      <Route exact path="/signin" component={SignIn} />
-      <Route path="*" component={NotFound} />
+      <Route exact path="/login" component={SignIn} />
+      <Redirect from="/projects*" to="/login" />
+      <Route component={NotFound} />
     </Switch>
   );
 };
