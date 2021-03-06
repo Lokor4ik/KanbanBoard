10411a5cec67a377f30c8bfd03dfab5b71e23739
Lokor4ik <lokorinfantem@gmail.com>
2021-03-06 16:22:55 +0200

    added Landing page

--- app/src/pages/Landing/Landing.tsx
+++ app/src/pages/Landing/Landing.tsx
@@ -1,14 +1,42 @@
+import { useHistory } from 'react-router-dom';
+
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+
 import MainLayout from 'layouts/MainLayout/MainLayout';
+import ColorButton from 'shared/Button/Button';
 
 import './Landing.scss';
 
-const NotFound = () => (
-  <MainLayout sectionName="landing">
-    <div className="landing__content">
-      <h1>Landing</h1>
-      <p>Buttons</p>
-    </div>
-  </MainLayout>
-);
+const useStyles = makeStyles({
+  tBody: {
+    marginBottom: '20px',
+  },
+});
 
-export default NotFound;
+const Landing = () => {
+  const history = useHistory();
+  const classes = useStyles();
+
+  const handleSignUp = () => {
+    history.push('/signup');
+  };
+
+  return (
+    <MainLayout sectionName="landing">
+      <div className="landing__content">
+        <Typography variant="h3" gutterBottom>
+          Welcome to Kanban board.
+        </Typography>
+
+        <Typography className={classes.tBody} variant="h5">
+          Sigh up now to start assigning tasks and keeping track of your progress.
+        </Typography>
+
+        <ColorButton onClick={handleSignUp}>Sign up-it&lsquo;s free!</ColorButton>
+      </div>
+    </MainLayout>
+  );
+};
+
+export default Landing;
