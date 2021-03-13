a99a18e457cda2e0483422beff1e2229c2ee99ea
Lokor4ik <lokorinfantem@gmail.com>
2021-03-13 18:10:33 +0200

    added handle action errors

---
+++ app/src/utils/actionErrors.ts
@@ -0,0 +1,11 @@
+import { Color } from '@material-ui/lab/Alert';
+
+const arr = [
+  { msg: 'Something went wrong', severity: 'error' },
+  { msg: 'Something went wrong', severity: 'error' },
+  { msg: 'Something went wrong', severity: 'error' },
+];
+const handleErrors = (error: Array<{ msg: string; severity: Color }> | Error) =>
+  error instanceof Array ? error : arr;
+
+export default handleErrors;
