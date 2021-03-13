005c51561a6a38a885d6f9e3c55ad667d59d28bc
Lokor4ik <lokorinfantem@gmail.com>
2021-03-13 18:08:20 +0200

    added notistack provider

--- app/src/index.tsx
+++ app/src/index.tsx
@@ -2,6 +2,8 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
 
+import { SnackbarProvider } from 'notistack';
+
 import store from 'store/store';
 
 import App from './App';
@@ -11,7 +13,9 @@ import './index.scss';
 ReactDOM.render(
   <Provider store={store}>
     <Router>
-      <App />
+      <SnackbarProvider maxSnack={4}>
+        <App />
+      </SnackbarProvider>
     </Router>
   </Provider>,
   document.getElementById('root')
