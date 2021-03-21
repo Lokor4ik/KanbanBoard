a0c164843c73bb10547f292c0db8e7e88be69fca
Lokor4ik <lokorinfantem@gmail.com>
2021-03-21 12:09:35 +0200

    deleted unnecessary component

--- app/src/shared/MessageAlert/MessageAlert.tsx
+++
@@ -1,5 +0,0 @@
-import MuiAlert, { AlertProps } from '@material-ui/lab/Alert';
-
-const MessageAlert = (props: AlertProps) => <MuiAlert elevation={6} variant="filled" {...props} />;
-
-export default MessageAlert;
