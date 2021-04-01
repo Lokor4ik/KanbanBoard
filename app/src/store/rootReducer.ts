6bbc6a564fa28f1bc22c928ee31f2b4b68d06092
Lokor4ik <lokorinfantem@gmail.com>
2021-04-01 12:15:15 +0300

    added new reducer

--- app/src/store/rootReducer.ts
+++ app/src/store/rootReducer.ts
@@ -2,10 +2,12 @@ import { combineReducers } from 'redux';
 
 import kanban from './kanban';
 import auth from './auth';
+import projects from './projects';
 
 const rootReducer = combineReducers({
   kanban: kanban.reducer,
   auth: auth.reducer,
+  projects: projects.reducer,
 });
 
 export default rootReducer;
