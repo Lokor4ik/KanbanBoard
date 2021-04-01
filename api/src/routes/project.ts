662a13e4f7c07a7f3f3447f8b12597ed65d59330
Lokor4ik <lokorinfantem@gmail.com>
2021-04-01 12:13:31 +0300

    added project route

---
+++ api/src/routes/project.ts
@@ -0,0 +1,26 @@
+import express from 'express';
+import { check } from 'express-validator';
+
+import projectController from 'controllers/project/project';
+import auth from 'middleware/auth/auth';
+
+const router = express.Router();
+
+const middlewares = [
+  auth,
+  check('name', 'Name is required').not().isEmpty(),
+  check('key', 'Key is required').not().isEmpty(),
+  check('lead', 'Lead is required').not().isEmpty(),
+];
+
+// @route  POST api/project
+// @desc   Create new project
+// @access Private
+router.post('/', middlewares, projectController.createProject);
+
+// @route  GET api/project
+// @desc   Get all assigned projects
+// @access Private
+router.get('/', auth, projectController.getAllProjects);
+
+export default router;
