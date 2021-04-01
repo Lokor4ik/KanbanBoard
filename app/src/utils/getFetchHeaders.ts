1f463fe5053aab5827e1c9c570d1c519a62ffd0e
Lokor4ik <lokorinfantem@gmail.com>
2021-04-01 15:23:38 +0300

    changed static code on func

--- app/src/utils/getFetchHeaders.ts
+++ app/src/utils/getFetchHeaders.ts
@@ -1,6 +1,6 @@
-const headers = {
+const getFetchHeaders = () => ({
   'Content-Type': 'application/json',
   'x-auth-token': localStorage.token,
-};
+});
 
-export default headers;
+export default getFetchHeaders;
