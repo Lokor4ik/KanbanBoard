943cd54dc78ed479b69e337ae26dc561bcee7f8c
Lokor4ik <lokorinfantem@gmail.com>
2021-04-06 22:30:42 +0300

    added new route

--- api/src/routes/user.ts
+++ api/src/routes/user.ts
@@ -1,19 +1,27 @@
 import express from 'express';
-import { check } from 'express-validator';
+import { check, query } from 'express-validator';
 
+import auth from 'middleware/auth/auth';
 import userController from 'controllers/user/user';
 
 const router = express.Router();
 
-const middlewares = [
+const middlewaresRegister = [
   check('name', 'Name is required').not().isEmpty(),
   check('email', 'Please include a valid email').isEmail(),
   check('password', 'Please enter a password with 6 or more characters').isLength({ min: 6 }),
 ];
 
-// @route  POST api/users
+const middlewaresfindByEmail = [auth, query('email', 'Email is required').isEmail().not().isEmpty()];
+
+// @route  POST api/user
 // @desc   Register user
 // @access Public
-router.post('/', middlewares, userController.register);
+router.post('/', middlewaresRegister, userController.register);
+
+// @route  GET api/user
+// @desc   Find user by email
+// @access Private
+router.get('/', middlewaresfindByEmail, userController.findByEmail);
 
 export default router;
