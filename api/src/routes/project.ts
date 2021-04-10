55e850fd824b8c8079cb3c455361d9c281556e3a
Lokor4ik <lokorinfantem@gmail.com>
2021-04-10 22:39:01 +0300

    improved route

--- api/src/routes/project.ts
+++ api/src/routes/project.ts
@@ -1,22 +1,24 @@
 import express from 'express';
-import { check } from 'express-validator';
+import { check, query } from 'express-validator';
 
 import projectController from 'controllers/project/project';
 import auth from 'middleware/auth/auth';
 
 const router = express.Router();
 
-const middlewares = [
+const middlewaresCreateProject = [
   auth,
   check('name', 'Name is required').not().isEmpty(),
   check('key', 'Key is required').not().isEmpty(),
   check('lead', 'Lead is required').not().isEmpty(),
 ];
 
+const middlewaresGetOneProject = [auth, query('id', 'Id is required').not().isEmpty()];
+
 // @route  POST api/project
 // @desc   Create new project
 // @access Private
-router.post('/', middlewares, projectController.createProject);
+router.post('/', middlewaresCreateProject, projectController.createProject);
 
 // @route  GET api/project
 // @desc   Get all assigned projects
@@ -26,6 +28,6 @@ router.get('/', auth, projectController.getAllProjects);
 // @route  GET api/project/:id
 // @desc   Get one project
 // @access Private
-router.get('/one', auth, projectController.getOneProject);
+router.get('/one', middlewaresGetOneProject, projectController.getOneProject);
 
 export default router;
